e74d8b43f91772ea4aa632db1a36baebbf43cfbd
Jebux <jebux25@hotmail.com>
2026-01-27 17:00:30 -0500

    Animated svg+react

---
+++ src/components/AnimatedReloj.jsx
@@ -0,0 +1,47 @@
+import "../styles/AnimatedReloj.css";
+export default function AnimatedReloj() {
+    return (
+        <div style={{height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+            <svg
+                width="400"
+                height="400"
+                viewBox="0 0 52.916665 52.916666"
+                version="1.1"
+                id="svg1"
+                className="icon"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:svg="http://www.w3.org/2000/svg">
+                <defs
+                    id="defs1" />
+                <g
+                    id="bg-reloj">
+                    <circle
+                        style={{ fontVariationSettings: "normal", opacity: 1, fill: "#f5f5f5", fillOpacity: 1, strokeWidth: 1.32291664, strokeLinecap: "butt", strokeLinejoin: "miter", strokeMiterlimit: 4, strokeDasharray: "none", strokeDashoffset: 0, strokeOpacity: 0.9813084, stopColor: "#000000", stopOpacity: 1, stroke: "#3230b2" }}
+                        id="path1"
+                        cx="26.461876"
+                        cy="25.009792"
+                        r="14.465" />
+                </g>
+                <g
+                    id="hand">
+                    <path
+                        style={{ fontVariationSettings: "normal", opacity: 1, fill: "none", fillOpacity: 1, stroke: "#3230b2", strokeWidth: 1.32292, strokeLinecap: "round", strokeLinejoin: "miter", strokeMiterlimit: 4, strokeDasharray: "none", strokeDashoffset: 0, strokeOpacity: 0.981308 }}
+                        d="M 26.399999,24.97 V 35.090001"
+                        id="path2" />
+                </g>
+                <g
+                    id="lines">
+                    <path
+                        style={{ fontVariationSettings: "normal", fill: "none", fillOpacity: 1, stroke: "#3230b2", strokeWidth: 1.32292, strokeLinecap: "round", strokeLinejoin: "miter", strokeMiterlimit: 4, strokeDasharray: "none", strokeDashoffset: 0, strokeOpacity: 0.981308 }}
+                        d="M 31.372694,7.4225994 21.252726,7.3968044"
+                        id="path2-2" />
+                    <path
+                        style={{ fontVariationSettings: "normal", fill: "none", fillOpacity: 1, stroke: "#3230b2", strokeWidth: 1.00758, strokeLinecap: "round", strokeLinejoin: "miter", strokeMiterlimit: 4, strokeDasharray: "none", strokeDashoffset: 0, strokeOpacity: 0.981308 }}
+                        d="m 36.207612,10.086737 5.400196,5.419447"
+                        id="path2-6" />
+                </g>
+            </svg>
+
+        </div>
+    )
+}
